4e5ab0a5d2fc5cf78d5b5bd7ffc1e3c677e611be
Javier Limonta Neyra <akabane02013@gmail.com>
2019-10-08 10:57:01 -0400

    Not SSL

--- app.js
+++ app.js
@@ -35,9 +35,9 @@ const store = new MongoDBStore({
 const csrfProtection = csrf();
 
 
-    const privateKey = fs.readFileSync('./server.key');
-    const certificate = fs.readFileSync('./server.cert');
-    const options = { key: privateKey, cert: certificate};
+    // const privateKey = fs.readFileSync('./server.key');
+    // const certificate = fs.readFileSync('./server.cert');
+    // const options = { key: privateKey, cert: certificate};
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
